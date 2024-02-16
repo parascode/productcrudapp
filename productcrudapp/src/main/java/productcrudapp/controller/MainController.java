784c21e7fede9a327af4c0bfddbe70a78d799777
parascode <jparas435@gmail.com>
2024-02-16 22:06:15 +0530

    added add product handler

--- productcrudapp/src/main/java/productcrudapp/controller/MainController.java
+++ productcrudapp/src/main/java/productcrudapp/controller/MainController.java
@@ -1,17 +1,39 @@
 package productcrudapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import productcrudapp.dao.ProductDao;
+import productcrudapp.model.Product;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	ProductDao productDao;
 
 	@RequestMapping("/")
 	public String home() {
 		return "index";
 	}
 	@RequestMapping("/add-product")
-	public String addProduct() {
+	public String addProduct(Model m) {
+		m.addAttribute("title", "Add Product");
 		return "add_product_form";
+	}
+	@RequestMapping(path = "handle-product", method = RequestMethod.POST)
+	public RedirectView handleProduct(@ModelAttribute Product product, HttpServletRequest request) {
+		productDao.createProduct(product);
+		System.out.println(product);
+		RedirectView redirectView = new  RedirectView();
+		redirectView.setUrl(request.getContextPath() + "/");
+		return redirectView;
 	}
 }
